b28de88467fa3562cef5334ffcf19c9c1d7d7333
Ayber Ulas Keskin <ayberulaskeskin@gmail.com>
2021-05-29 15:56:24 +0300

    tenary experiment

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,9 @@
+let ayber="ayber"
+let info =document.querySelector("#info")
+let userName=prompt("Kullanıcı adınız")
 
 
-let main=document.querySelector("#main")
-main.classList.add("background")
 
-
-var randomSayi = Math.floor(Math.random() * 10);
-
-//Girdiğimiz sayıyı aklında tutması için prompt değişkene atanır
-
-var tahmin = prompt("1-10'a kadar bir sayı gir ve rastgele sayıyı bulmaya çalış!");
-
-if (tahmin === randomSayi && tahmin != null) alert("Bildin!!!");
-else if (tahmin == "") alert("Boş giriş yaptınız!");
-else if (tahmin == null) alert("Giriş yapmaktan vazgeçtiniz!");
-else {
-  alert("Bir daha denee :(, Random sayi: " + randomSayi);
-}
+  info.innerHTML=`${userName.length>0 ? userName :"Kullancı adı bulunamadı"}`
 
 
