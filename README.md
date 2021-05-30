0da8b34b3099e888cf5fae7f04b0b91fc4e03d68
Ayber Ulas Keskin <ayberulaskeskin@gmail.com>
2021-05-31 00:45:32 +0300

    javascript01 homework

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# javascript-examples-01
-learning javascript with examples from patika.dev 
+# javascript-homework-01
+## This is Javascript homework 1 from React Native with Mobile App Patika from [Patika](https://patika.dev)
+### Html and Css codes are taken from this [link](https://github.com/Kodluyoruz/taskforce/tree/javascript/javascript/javascript-temel/odev1/javascriptclock).
--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,28 @@
-let ayber="ayber"
-let info =document.querySelector("#info")
-let userName=prompt("Kullanıcı adınız")
+
+let myName = prompt("İsminiz nedir?");
+while(myName==="")
+{
+   myName = prompt("İsminizi girmediniz!");
+}
+
+document.querySelector("#myName").innerText = myName;
 
 
+function showTime() {
+ let date = new Date();
+ let h = date.getHours();
+ let m = date.getMinutes();
+ let s = date.getSeconds();
+ let day = date.getDay();
+let arr =["Pazar","Pazartesi", "Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
+let b =date.getSeconds();
 
-  info.innerHTML=`${userName.length>0 ? userName :"Kullancı adı bulunamadı"}`
+ h = h < 10 ? "0" + h : h;
+ m = m < 10 ? "0" + m : m;
+ s = s < 10 ? "0" + s : s;
 
-
+ let time = h + ":" + m + ":" + s + " " + arr[day];
+ document.querySelector("#myClock").innerHTML = time;
+ setTimeout(showTime, 1000);
+}
+showTime();
